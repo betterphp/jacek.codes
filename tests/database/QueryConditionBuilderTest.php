1847c56d6cfa2c585b7afb47e8a0f16114005b55
Jacek Kuzemczak <jacek@kuzemczak.co.uk>
2017-10-03 22:27:53 +0100

    Added more tests for query builder

--- tests/database/QueryConditionBuilderTest.php
+++ tests/database/QueryConditionBuilderTest.php
@@ -26,4 +26,107 @@ class QueryConditionBuilderTest extends TestCase {
         $this->assertSame($expected_offset, $actual_offset);
     }
 
+    public function testGetSql(): void {
+        $expected_sql = 'SELECT such, colum, very, relational FROM doge';
+
+        $builder = new query_condition_builder();
+
+        reflection::set_property($builder, 'sql', $expected_sql);
+
+        $this->assertSame($expected_sql, $builder->get_sql());
+    }
+
+    public function testGetParams(): void {
+        $expected_params = [
+            'such' => 'params',
+            'very' => 'assoc',
+        ];
+
+        $builder = new query_condition_builder();
+
+        reflection::set_property($builder, 'params', $expected_params);
+
+        $this->assertSame($expected_params, $builder->get_params());
+    }
+
+    public function testGetParamName(): void {
+        $field = 'very_field';
+        $offset = 10;
+        $expected_field_name = "{$field}_{$offset}";
+
+        $builder = new query_condition_builder($offset);
+
+        $actual_field_name = reflection::call_method($builder, 'get_param_name', [$field]);
+
+        $this->assertSame($expected_field_name, $actual_field_name);
+    }
+
+    /**
+     * @dataProvider dataGetcomparisonString
+     */
+    public function testGetComparisonString(
+        string $comparison,
+        $value,
+        string $expected_string
+    ): void {
+        $builder = new query_condition_builder();
+
+        $actual_string = reflection::call_method(
+            $builder,
+            'get_comparison_string',
+            [
+                'field',
+                $comparison,
+                $value,
+            ]
+        );
+
+        $this->assertSame($expected_string, $actual_string);
+    }
+
+    public function dataGetcomparisonString(): array {
+        return [
+            ['=', [0, 1, 2], ' IN (:field_0,:field_1,:field_2) '],
+            ['!=', [0, 1], ' NOT IN (:field_0,:field_1) '],
+            ['=', 'anything', ' = :field_0 '],
+            ['!=', 'anything', ' != :field_0 '],
+            ['>', 'anything', ' > :field_0 '],
+            ['<', 'anything', ' < :field_0 '],
+        ];
+    }
+
+    /**
+     * @dataProvider dataGetComparisonStringWithInvalidInput
+     */
+    public function testGetComparisonStringWithInvalidInput(
+        string $comparison,
+        $value,
+        string $expected_message
+    ): void {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage($expected_message);
+
+        $builder = new query_condition_builder();
+
+        reflection::call_method(
+            $builder,
+            'get_comparison_string',
+            [
+                'field',
+                $comparison,
+                $value,
+            ]
+        );
+    }
+
+    public function dataGetComparisonStringWithInvalidInput(): array {
+        return [
+            ['=', [], 'Comparison with an empty list'],
+            ['what even', [1, 2, 3], 'Unsupported comparison'],
+            ['>', null, 'Greater than comparison with null is not possible'],
+            ['<', null, 'Less than comparison with null is not possible'],
+            ['what even', 'anything', 'Unsupported comparison'],
+        ];
+    }
+
 }
