a6735f72ba9fd2362cc333276ec678911822c6cb
Jacek Kuzemczak <jacek@kuzemczak.co.uk>
2017-10-08 16:14:17 +0100

    Added method to get a map of the most recent commits in all git repos

--- src/model/activity/git_commit.php
+++ src/model/activity/git_commit.php
@@ -15,4 +15,35 @@ class git_commit extends activity {
         'id' => 'int',
     ];
 
+    /**
+     * Gets a map of the last imported commit date for all git repos
+     *
+     * @return array The map
+     */
+    public static function get_last_commit_dates(string $host_domain): array {
+        $sql = <<<SQL
+            SELECT
+                project_name,
+                MAX(date) AS date
+            FROM git_commit
+            WHERE host_domain = :host_domain
+            GROUP BY project_name
+SQL;
+
+        $database = self::get_database();
+        $stmt = $database->prepare($sql);
+
+        $stmt->bindParam(':host_domain', $host_domain, \PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        $repos = [];
+
+        while ($row = $stmt->fetch()) {
+            $repos[$row->project_name] = \DateTime::createFromFormat('Y-m-d H:i:s', $row->date);
+        }
+
+        return $repos;
+    }
+
 }
